c5bd58e52ffc926d4146e982aa57f9ffba7da658
Ziana Aulia <zianaaulia2602@gmail.com>
2022-03-16 21:30:02 +0700

    Kode Program Keenambelas

---
+++ src/main/kotlin/Elemen Program Kotlin/16.  String (Array of Character).kt
@@ -0,0 +1,14 @@
+package `Elemen Program Kotlin`
+
+fun main(args: Array<String>) {
+    val myName: String = "Ziana Aulia"
+    for(chr in myName){
+        print(chr)
+    }
+    print('\n')
+}
+
+/*
+String merupakan array dari karakter dan bersifat immutable (bersifat statis). Elemen suatu
+String dapat diakses dengan operasi indeks array contoh s[i] atau dengan menggunakan for-loop.
+ */
