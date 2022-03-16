307d935dcfbeec76ca5c63bdbfbf096e3e37b91e
Ziana Aulia <zianaaulia2602@gmail.com>
2022-03-16 20:54:19 +0700

    Kode Program Kedelapan

---
+++ src/main/kotlin/Elemen Program Kotlin/8. Tipe Data Bilangan Decimal.kt
@@ -0,0 +1,9 @@
+package `Elemen Program Kotlin`
+
+fun main(args: Array<String>) {
+    println("Penggunaan Tipe Data bilangan Desimal (Floating-point)")
+    val maxFloat: Float = 9.123456789f //Penggunaan akhiran f atau F pada akhir literal agar nilai menjadi float
+    val maxDouble: Double = 9.123456789
+    println("maxFloat: " + maxFloat)
+    println("maxDouble: " + maxDouble)
+}
