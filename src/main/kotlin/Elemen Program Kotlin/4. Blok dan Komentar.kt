bb743ad48aa36e86ceb792f33db5d3542acab518
Ziana Aulia <zianaaulia2602@gmail.com>
2022-03-16 22:24:05 +0700

    Kode Program Keempat untuk Elemen Program Kotlin

---
+++ src/main/kotlin/Elemen Program Kotlin/4. Blok dan Komentar.kt
@@ -0,0 +1,14 @@
+package `Elemen Program Kotlin`
+
+fun main(args: Array<String>) {
+    println("Hello, ini program pertama saya di Kotlin!")
+    //Blok ditandai dengan tanda buka dan tutup kurung kurawal ({})
+}
+//Komentar
+//Contoh Kometar satu baris
+/*
+Contoh Komentar multi baris
+ */
+/**
+ * Kotlin Doc --> baris ini untuk mendokumentasikan kode program
+ */
