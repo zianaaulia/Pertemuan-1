d4368e3a2c1853f204db00ea64fe5a1aab7498a5
Ziana Aulia <zianaaulia2602@gmail.com>
2022-03-16 21:26:34 +0700

    Kode Program Kelimabelas

--- src/main/kotlin/Elemen Program Kotlin/15. Boolean (true or false).kt
+++ src/main/kotlin/Elemen Program Kotlin/15. Boolean (true or false).kt
@@ -4,6 +4,16 @@ fun main(args: Array<String>) {
     var varBool : Boolean = true
     if(varBool)
         println("varBool bernilai " + varBool)
+
+    /*
+    Kode dibawah ini salah karena type yang diinginkan boolean
+    namun yang ditemui type int
+     */
+/*
+var varInt : Int = 1
+if(varInt)
+println("varInt bernilai " + varInt)
+*/
 }
 
 /*
