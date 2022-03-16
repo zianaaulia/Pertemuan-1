3181b19074a5e7c27a7a59ea96a0b3a06c834c38
Ziana Aulia <zianaaulia2602@gmail.com>
2022-03-16 20:52:49 +0700

    Kode Program Ketiga

---
+++ src/main/kotlin/Elemen Program Kotlin/3. Whitespaces.kt
@@ -0,0 +1,9 @@
+package `Elemen Program Kotlin`
+
+fun main() {
+    println("Hello, world!") }
+
+/*
+    Whitespace tidak signifikan mempengaruhi program
+    dan dengan aman dapat diacuhkan.
+ */
