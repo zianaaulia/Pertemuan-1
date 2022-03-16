3324515432bcdd3327675c20cfa1f8e8f20e929b
Ziana Aulia <zianaaulia2602@gmail.com>
2022-03-16 20:53:45 +0700

    Kode Program Keenam

---
+++ src/main/kotlin/Elemen Program Kotlin/6. Var vs Val.kt
@@ -0,0 +1,19 @@
+package `Elemen Program Kotlin`
+
+fun main(args: Array<String>) {
+    var varVariabel : Int
+    varVariabel = 56 // Menetapkan nilai pertama pada varVariabel
+    println("Pemberian nilai pertama pada varVariabel = " + varVariabel)
+    varVariabel = 78 // Menetapkan nilai kedua pada varVariabel
+    println("Pemberian nilai kedua pada varVariabel = " + varVariabel)
+    val phi = 3.14
+
+    //phi = 2.15
+
+    /*
+    Memasukkan nilai phi sebanyak 2 kali akan
+    menimbulkan error jika menggunakan val variabel dan
+    akan memberikan pesan kesalahan untuk mengubah val variabel ke var variabel
+     */
+
+}
