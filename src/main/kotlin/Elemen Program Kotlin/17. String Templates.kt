e30660b2e900fc3b2ff3e338c5e4fdffa1abf00a
Ziana Aulia <zianaaulia2602@gmail.com>
2022-03-16 21:39:55 +0700

    Kode Program Ketujuhbelas

---
+++ src/main/kotlin/Elemen Program Kotlin/17. String Templates.kt
@@ -0,0 +1,20 @@
+package `Elemen Program Kotlin`
+
+fun main(args: Array<String>) {
+    val a = 7
+    val b = 8
+    val c = a + b
+    val s = "Ketika kita tambah $a dan $b, maka akan dapat $c"
+    println(s)
+    println("Ketika kita tambah $a dan $b, maka akan dapat ${a + b}")
+    val price = """
+    ${'$'}9.99
+    """
+    println(price)
+}
+
+/*
+Diperlukan tanda kurung kurawal untuk melakukan pemrosesan pada saat menggabungkan
+variabel kedalam template String. Karakter dolar („$‟) tidak bisa dicetak menggunakan escape
+character oleh karena itu gunakan template String untuk mencetaknya.
+ */
