5b5a3f9ff7b2b5a53f0c0dd82442a67618a05ec5
Ziana Aulia <zianaaulia2602@gmail.com>
2022-03-16 21:36:39 +0700

    Kode Program Keenambelas

--- src/main/kotlin/Elemen Program Kotlin/16.  String (Array of Character).kt
+++ src/main/kotlin/Elemen Program Kotlin/16.  String (Array of Character).kt
@@ -6,9 +6,37 @@ fun main(args: Array<String>) {
         print(chr)
     }
     print('\n')
+    /*
+    Kita bisa menggabungkan String dengan tipe data lain dengan menggunakan operator +,
+    sehingga tipe data yang digabung otomatis akan melebur kedalam String.
+    */
+    val s = "abc" + 1
+    println(s + "def")
+    /*
+    Untuk dapat menyimpan data String yang memuat keywords Kotlin bisa dengan menggunakan
+    raw String yang dipisahkan dengan triple quotes (""").
+     */
+    var text1 = """
+        for (c in "foo")
+        print(c)
+    """
+    println(text1)
+    /*
+    Kita juga dapat menghapus spasi yang tidak diperlukan dalam raw String seperti contoh kode
+    program berikut:
+     */
+    var text2 = """
+            |Tell me and I forget.
+            |Teach me and I remember.
+            |Involve me and I learn.
+            |(Benjamin Franklin)
+            """.trimMargin()
+    println(text2)
 }
 
 /*
 String merupakan array dari karakter dan bersifat immutable (bersifat statis). Elemen suatu
 String dapat diakses dengan operasi indeks array contoh s[i] atau dengan menggunakan for-loop.
  */
+
+
