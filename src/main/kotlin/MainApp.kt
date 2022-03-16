b1ae9612ac8ca42a2b195433eaa5f61207c2afa9
Ziana Aulia <zianaaulia2602@gmail.com>
2022-03-16 20:38:34 +0700

    Kode Program Pertama; melakukan fungsi dasar yaitu mencetak string.

---
+++ src/main/kotlin/MainApp.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    println("Hello, Ini Program Pertama Saya di Kotlin")
+}
+
+/*
+    *fun adalah sebuah keyword yang menandakan sebuah fungsi di kotlin
+    *fun main() adalah fungsi main di aplikasi bahasa Kotlin yang digunakan
+      sebagai titik awal ketika program dijalankan.
+    *println() adalah fungsi dasar untuk mencetak string
+ */
