89e4fbd158a98fe5de9531bbefe5a2e8827f4d81
Ziana Aulia <zianaaulia2602@gmail.com>
2022-03-16 21:14:10 +0700

    Kode Program Ketigabelas

---
+++ src/main/kotlin/Elemen Program Kotlin/13. Character(Char).kt
@@ -0,0 +1,28 @@
+package `Elemen Program Kotlin`
+
+
+/*
+fun main(args: Array<String>) {
+var charA : Char = 'A'
+println(charA == 65)
+}
+*/
+
+/*
+Terdapat error pada kode program diatas karena penggunaan
+operator == tidak bisa digunakan pada tipe data char dan integer
+*/
+
+//Kode Program penggunaan fungs anggota dari Character:
+
+fun main(args: Array<String>) {
+    var charA : Char = 'A'
+//println(charA == 65)
+    println(charA.isUpperCase())
+    println(charA.isLowerCase())
+    println(charA.isDigit())
+    println(charA.toLowerCase())
+    val strA: String = charA.toString()
+    println("Kini charA sudah menjadi String = "+strA)
+}
+
