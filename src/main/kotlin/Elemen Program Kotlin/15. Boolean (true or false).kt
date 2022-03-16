1a887aa1d326d0b8ba144d77b11eaa39d5ea1831
Ziana Aulia <zianaaulia2602@gmail.com>
2022-03-16 21:22:43 +0700

    Kode Program Kelimabelas

---
+++ src/main/kotlin/Elemen Program Kotlin/15. Boolean (true or false).kt
@@ -0,0 +1,17 @@
+package `Elemen Program Kotlin`
+
+fun main(args: Array<String>) {
+    var varBool : Boolean = true
+    if(varBool)
+        println("varBool bernilai " + varBool)
+}
+
+/*
+Tipe Boolean mewakili objek boolean yang dapat memiliki dua nilai: benar dan salah.
+Boolean memiliki mitra nullable Boolean? yang juga memiliki nilai nol.
+Operasi bawaan pada boolean meliputi:
+|| – disjungsi (logis ATAU)
+&& – konjungsi (logis DAN)
+! - negasi (tidak logis)
+|| dan && bekerja dengan malas.
+ */
