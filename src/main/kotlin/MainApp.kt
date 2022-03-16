2927d12de4514bac65801f900a6507f094920134
Ziana Aulia <zianaaulia2602@gmail.com>
2022-03-16 20:51:56 +0700

    Tugas Pra-Praktikum

--- src/main/kotlin/MainApp.kt
+++ src/main/kotlin/MainApp.kt
@@ -1,7 +1,6 @@
 fun main(args: Array<String>) {
     println("Hello, Ini Program Pertama Saya di Kotlin")
 }
-
 /*
     *fun adalah sebuah keyword yang menandakan sebuah fungsi di kotlin
     *fun main() adalah fungsi main di aplikasi bahasa Kotlin yang digunakan
