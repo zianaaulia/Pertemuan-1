d8ecf206735cbc81f48cda5b40ba98ee6875abd6
Ziana Aulia <zianaaulia2602@gmail.com>
2022-03-16 20:53:25 +0700

    Kode Program Kelima

---
+++ src/main/kotlin/Elemen Program Kotlin/5. Variabel.kt
@@ -0,0 +1,16 @@
+package `Elemen Program Kotlin`
+
+fun main(args: Array<String>) {
+    var dynamicVar = 2
+    println("Contoh dynamic variabel = " + dynamicVar)
+
+    var staticVar: Char = 'A'
+    println("Contoh static variabel = " + staticVar)
+}
+
+
+/*
+Variabel merupakan sebuah tempat pada memori yang
+digunakan untuk menyimpan data.
+Terdapat 2 system variabel yaitu dinamis dan statis
+ */
