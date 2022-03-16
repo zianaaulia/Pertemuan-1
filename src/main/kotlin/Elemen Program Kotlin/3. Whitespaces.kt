5181fb2f934a6d9340372972815518ec2735e8cd
Ziana Aulia <zianaaulia2602@gmail.com>
2022-03-16 22:23:50 +0700

    Kode Program Ketiga untuk Elemen Program Kotlin

---
+++ src/main/kotlin/Elemen Program Kotlin/3. Whitespaces.kt
@@ -0,0 +1,9 @@
+package `Elemen Program Kotlin`
+
+fun main() {
+    println("Hello, world!") }
+
+/*
+Seperti Java, Kotlin juga merupakan bahasa pemrograman yang ditokenisasi. Whitespace tidak
+signifikan mempengaruhi program dan dengan aman dapat diacuhkan.
+ */
