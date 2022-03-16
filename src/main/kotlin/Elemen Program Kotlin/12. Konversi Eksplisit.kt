9d3fb52608df9d2365701f6cd8d88c438916a928
Ziana Aulia <zianaaulia2602@gmail.com>
2022-03-16 21:10:12 +0700

    Kode Program Keduabelas

---
+++ src/main/kotlin/Elemen Program Kotlin/12. Konversi Eksplisit.kt
@@ -0,0 +1,21 @@
+package `Elemen Program Kotlin`
+
+fun main(args: Array<String>) {
+    val valInt: Int = 1
+    val valSum: Long = 3L + valInt //konversi implisit
+    println("Konversi variabel valInt secara implisit : " + valSum)
+    val valLong: Long = valInt.toLong() // konversi eksplisit
+    println("Konversi variabel valInt secara eksplisit : " + valLong)
+}
+
+/*
+ konversi secara implisit adalah ketika compiler mampu melakukan konversi
+ tanpa perlu diberi instruksi apapun untuk melakukan konversi.
+ Tidak semua konversi bisa dilakukan secara implisit
+ */
+
+/*
+Konversi tipe ini dilakukan secara manual oleh sang programmer,
+beda dengan implisit yang secara otomatis terkonversi oleh compiler.
+Metode ini boleh atau biasa disebut type casting
+ */
